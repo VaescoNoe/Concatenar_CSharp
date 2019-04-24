b684079d7f63d8852da449e98c939cffbff3189d
Noe <VaescoNoe@gmail.com>
2019-04-23 22:58:40 -0500

    Concatenar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        string mundo = "Mundo";
+        int dos = 2;
+         
+        Console.WriteLine("Hola " + "Mundo "+ dos);
+        Console.WriteLine("Hola {0} {1}",mundo,dos);
+        Console.WriteLine($"Hola {mundo} {dos}");
+
+        }
+    }
+
